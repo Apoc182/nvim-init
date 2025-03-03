0c218dec555e866d19fcf200771b7d3d468fa7f8
Justin Lillico <justin.lillico@outbackequipment.com.au>
2025-03-03 13:55:33 +1100

    Added loading of project leevel lua to the init config

--- init.lua
+++ init.lua
@@ -2,6 +2,19 @@
 require("keymaps")  -- loads lua/keymaps.lua
 require("options")  -- loads lua/options.lua
 
+
+-- This is to allow local configs to be loaded
+vim.opt.exrc = true
+vim.opt.secure = true
+
+-- Mapping for yanking to system clipboard
+vim.keymap.set('v', '<leader>y', '\"*y', { noremap = true, desc = 'Yank to system clipboard' })
+vim.keymap.set('n', '<leader>y', '\"*y', { noremap = true, desc = 'Yank to system clipboard' })
+
+-- Mapping for pasting from system clipboard
+vim.keymap.set('n', '<leader>p', '\"*p', { noremap = true, desc = 'Paste from system clipboard' })
+vim.keymap.set('v', '<leader>p', '\"*p', { noremap = true, desc = 'Paste from system clipboard' })
+
 -- Reload init.lua
 local function reload_init()
     vim.cmd("source $MYVIMRC") -- Reloads the main configuration file
